ad914d5c12d9d9eeb2e0e48223d9c909209f54dc
skyeler yearsley <s2krighty@gmail.com>
2023-06-16 22:13:44 -0500

    Update of weather class and addition of day class

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace LemonadeStand
 {
     internal class Weather
     {
+        Random random;
+        public string conditions;
+        public int temperature;
+        private List<string> weatherconditions;
+        public string predictiedForcast;
+
+        public Weather(Random random)
+
+        {
+            this.random = random;
+            temperature = (0);
+            weatherconditions = new List<string> { "Mostly sunny", "mostly cloudy", "Sunny", "Cloudy", "Rainy", "Partly cloudly", "Sunny rainy" };
+            
+        }
+
+
+        public List<string> WeatherConditions 
+        
+        {
+            get
+            {
+                return weatherconditions;
+            }
+            set
+            {
+                weatherconditions = value;
+
+            }
+       
+        
+        }
+
+        
+
+
+
     }
 }
